fc6170b4cd804dcf52299f540b8cd5d971445f58
StefanDsd <stefan_dascalescu92@gmail.com>
2023-01-05 12:07:50 +0100

    retrieving data by filtered params

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,16 +1,69 @@
 const database = require("./database");
 
+
+// WORKING ON EXPRESS QUEST 06 //
 const getMovies = (req, res) => {
-  database
-    .query("select * from movies")
+
+// let sql = " select * from movies"
+// const sqlValues = [];
+
+// if(req.query.color != null) {
+//   sql += " where color = ?";
+//   sqlValues.push(req.query.color);
+// }
+// if(req.query.max_duration != null) {
+//   sql += " where duration >?";
+//   sqlValues.push(req.query.max_duration);
+// } 
+// else if (req.query.max_duration != null) {
+// sql += " where duration <= ?";
+// sqlValues.push(req.query.max_duration);
+// }
+const initialSql = "select * from movies";
+const where = [];
+if (req.query.color != null) {
+  where.push({
+    column: "color",
+    value: req.query.color,
+    operator: "=",
+  });
+}
+if (req.query.max_duration != null) {
+  where.push({
+      column: "duration",
+      value: req.query.max_duration,
+      operator: "<=",
+    });
+  
+}
+database
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
     .then(([movies]) => {
       res.json(movies);
-      console.log(movies);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("error retrieving data from database");
+      res.status(500).send("Error retrieving data from database");
     });
+
+
+  // database
+  //   .query(sql, sqlValues)
+  //   .then(([movies]) => {
+  //     res.json(movies);
+      
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //     res.status(500).send("error retrieving data from database");
+  //   });
 };
 
 const getMovieById = (req, res) => {
@@ -30,6 +83,9 @@ const getMovieById = (req, res) => {
       res.status(404).send("error retrieving data from database");
     });
 };
+
+
+
 
 const postMovie = (req, res) => {
   const { title, director, year, color, duration } = req.body;
